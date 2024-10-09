75a4f40592981d4eef5e5bf47a0da63884a989fd
vitorbizarra <eu@vitorbizarra.dev.br>
2024-10-08 22:04:16 -0300

    wip: index

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,3 +1,13 @@
-<div>
-    Hello, World!
+<div class="h-svh w-full px-8 py-16 lg:p-16">
+    <div class="w-full h-full border p-2 flex flex-col justify-center items-center text-center border-zinc-900 text-zinc-900 dark:border-zinc-100 dark:text-zinc-100">
+        <h1 class="text-3xl lg:text-6xl font-bold font-mono mb-2">
+            Vitor Bizarra
+        </h1>
+        <h2 class="text-lg lg:text-3xl font-medium font-sans mb-2">
+            Fullstack Developer
+        </h2>
+        <h3 class="text-sm lg:text-lg font-mono">
+            PHP | Laravel | Livewire
+        </h3>
+    </div>
 </div>
--- resources/views/livewire/web/components/navbar.blade.php
+++ resources/views/livewire/web/components/navbar.blade.php
@@ -1,21 +1,19 @@
-<div>
-    <div class="py-4 px-4 lg:px-8 mx-auto flex justify-between text-zinc-900 dark:text-zinc-100">
-        <a href="#" class="font-mono font-medium text-lg">
-            {{ config('app.name') }}
-        </a>
+<div class="absolute top-0 w-full py-4 px-4 lg:px-16 mx-auto flex justify-between bg-zinc-100 text-zinc-900 dark:text-zinc-100 dark:bg-zinc-900">
+    <a href="#" class="font-mono font-medium text-lg">
+        {{ config('app.name') }}
+    </a>
 
-        <button 
-            type="button"
-            x-on:click="darkTheme = !darkTheme">
-            <x-heroicon-m-moon
-                x-show="darkTheme"
-                x-cloak
-                class="h-6 w-6 text-zinc-100" />
+    <button 
+        type="button"
+        x-on:click="darkTheme = !darkTheme">
+        <x-heroicon-m-moon
+            x-show="darkTheme"
+            x-cloak
+            class="h-6 w-6 text-zinc-100" />
 
-            <x-heroicon-m-sun 
-                x-show="!darkTheme"
-                x-cloak
-                class="h-6 w-6 text-zinc-900" />
-        </button>
-    </div>
+        <x-heroicon-m-sun 
+            x-show="!darkTheme"
+            x-cloak
+            class="h-6 w-6 text-zinc-900" />
+    </button>
 </div>
